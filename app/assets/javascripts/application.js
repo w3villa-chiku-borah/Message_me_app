47178675bb497a90542f42c52859f10ca826bedd
CHIKU BORAH <chiku.borah@w3villa.com>
2023-05-12 15:42:05 +0530

    add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(function(){
+    $('.ui.dropdown').dropdown();
+  })
